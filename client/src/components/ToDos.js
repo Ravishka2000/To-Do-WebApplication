cad41b07a92cb3c6866af1cf4ac4ebdb7e14cf04
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-02-08 20:09:17 +0530

    create view todo page

--- client/src/components/ToDos.js
+++ client/src/components/ToDos.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const ToDos = () => {
-  return (
-    <div>
-      <h1>hhh</h1>
-    </div>
-  )
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8090/')
+            .then(res => {
+                setTodos(res.data.todos);
+            })
+            .catch(res => { alert(res.data.message) });
+    }, [])
+
+    return (
+        <div>
+            <table className="table table-striped">
+                <thead>
+                    <td>Title</td>
+                    <td>Descriptioin</td>
+                    <td>Due Date</td>
+                    <td>Status</td>
+                </thead>
+                <tbody>
+                    {todos.map(todo => (
+                        <tr key={todo.id}>
+                            <td>{todo.title}</td>
+                            <td>{todo.description}</td>
+                            <td>{todo.dueDate}</td>
+                            {todo.completed ? <td>Completed</td> : <td>Not Completed</td>}
+                            
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default ToDos
